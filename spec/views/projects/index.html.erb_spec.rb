fa30083e19baeb6c3e523f40a5fbed2005bdf0cb
munim90 <munim.iftikhar@educative.io>
2021-02-26 12:55:46 +0500

    changeindexfile

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -1,17 +1,50 @@
-require "rails_helper"
+require 'rails_helper'
+describe 'projects/index' do
+  let(:on_schedule) do
+    Project.create!(
+      due_date: 1.year.from_now,
+      name: 'On Schedule'
+    )
+  end
 
-describe "projects/index" do
-    let(:completed_task) { Task.create!(completed_at: 1.day.ago, size: 1) }
-    let(:on_schedule) { Project.create!(
-    due_date: 1.year.from_now, name: "On Schedule", tasks: [completed_task]) }
-    let(:incomplete_task) { Task.create!(size: 1) }
-    let(:behind_schedule) { Project.create!(
-    due_date: 1.day.from_now, name: "Behind Schedule",
-    tasks: [incomplete_task]) }
-    it "renders the index page with correct dom elements" do
-        @projects = [on_schedule, behind_schedule]
-        render
-        expect(rendered).to have_selector("#project_#{on_schedule.id} .on_schedule")
-        expect(rendered).to have_selector("#project_#{behind_schedule.id} .behind_schedule")
+  let(:behind_schedule) do
+    Project.create!(
+      due_date: 1.day.from_now,
+      name: 'Behind Schedule'
+    )
+  end
+
+  let(:completed_task) do
+    Task.create!(
+      completed_at: 1.day.ago,
+      size: 1,
+      project: on_schedule
+    )
+  end
+
+  let(:incomplete_task) do
+    Task.create!(
+      size: 1,
+      project: behind_schedule
+    )
+  end
+
+  it 'renders the index page with correct dom elements' do
+    on_schedule.tasks << completed_task
+    behind_schedule.tasks << incomplete_task
+
+    @projects = [on_schedule, behind_schedule]
+    render
+    within("#project_#{on_schedule.id}") do
+      expect(rendered).to have_selector('.on_Schedule')
     end
+
+    expect(rendered).to have_selector(
+      "#project_#{on_schedule.id} .on_schedule"
+    )
+
+    expect(rendered).to have_selector(
+      "#project_#{behind_schedule.id} .behind_schedule"
+    )
+  end
 end
